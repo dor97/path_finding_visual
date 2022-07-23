264d99a65ddcf4133aa8bbd6a9c30d439b29b7a0
dor97 <88962398+dor97@users.noreply.github.com>
2022-07-23 18:01:24 +0300

    Update algorithm_bfs.py

--- algorithm/algorithm_bfs.py
+++ algorithm/algorithm_bfs.py
@@ -1,24 +1,22 @@
 import pygame
 from queue import Queue
 
-# RED = (255, 0, 0)
-# GREEN = (0, 255, 0)
-# BLUE = (0, 255, 0)
-# YELLOW = (255, 255, 0)
-# WHITE = (255, 255, 255)
-# BLACK = (0, 0, 0)
-# PURPLE = (128, 0, 128)
-# ORANGE = (255, 165 ,0)
-# GREY = (128, 128, 128)
-# TURQUOISE = (64, 224, 208)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 
 def Algorithm_bfs(draw, grid, start, end):
     que = Queue()
     come_from = {}
     que.put(start)
-    #map = {spot : 1 for row in grid for spot in row}
-    #map[start] = 0
     while not que.empty():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -32,7 +30,7 @@ def Algorithm_bfs(draw, grid, start, end):
             return True
 
         for neighbor in current.neighbors:
-            if neighbor.is_open(): #map[neighbor] == 1 and 
+            if neighbor.is_open(): 
                 come_from[neighbor] = current
                 neighbor.make_stat(GREEN)
                 que.put(neighbor)
